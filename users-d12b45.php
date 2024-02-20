33a5f573211b95d1752b38b498ed8b04760ba4c1
Shea Patterson <sheapatterson@icloud.com>
2024-02-20 12:13:55 -0500

    update

--- users-d12b45.php
+++ users-d12b45.php
@@ -1,3 +1,3 @@
 <?php
-    $users = array('shea' => array('id' => 1, 'name' => 'Shea', 'surname' => 'Patterson', 'pwd' => 'OJT9hMvUmWFmFKaPURSc97s3VjZ4tRR.', 'email' => 'sheapatterson@icloud.com', 'info' => '', 'groups' => array('0')), 'testuser' => array('id' => 2, 'name' => '', 'surname' => '', 'pwd' => 'OJT9hMvUmWFmFKaPURSc97s3VjZ4tRR.', 'email' => '', 'info' => '', 'groups' => array('1')));
-    $salt = '$2y$10$j2xQDnrlvrMmpmlbrE3wab$';
+    $users = array('shea' => array('id' => 1, 'name' => 'Shea', 'surname' => 'Patterson', 'pwd' => 'etLqHbC1WiSrJFl7Mo3WSr3IJKoTZgBW', 'email' => 'sheapatterson@icloud.com', 'info' => '', 'groups' => array('0')), 'testuser' => array('id' => 2, 'name' => '', 'surname' => '', 'pwd' => 'etLqHbC1WiSrJFl7Mo3WSr3IJKoTZgBW', 'email' => '', 'info' => '', 'groups' => array('1')));
+    $salt = '$2y$10$eylCjpvpc46nqucYLetaOm$';
